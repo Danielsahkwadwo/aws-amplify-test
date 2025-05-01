93db371a46ce33150f93a60c642343d211a1907d
Danielsahkwadwo <daniel.sah@amalitech.com>
2025-05-01 11:40:31 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_GSVyZg1cR', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '60t8gov1igmu638jd6g6a2tjl4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
